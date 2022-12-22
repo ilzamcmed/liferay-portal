ceb03efbce136a6ef0bc6023964379b201faa348
Ilza Medeiros <ilza.medeiros@liferay.com>
2022-12-22 14:27:29 -0300

    LRAC-12639 - Create unit  tests for People Page

---
+++ modules/apps/analytics/analytics-settings-web/src/main/resources/META-INF/resources/js/pages/default/tests/PeoplePage.spec.tsx
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import React from 'react';
+
+import '@testing-library/jest-dom/extend-expect';
+import {render} from '@testing-library/react';
+
+import PeoplePage from '../PeoplePage';
+
+describe('PeoplePage', () => {
+	it('renders page title and description', () => {
+		const {getByText} = render(<PeoplePage title="People" />);
+
+		const title = getByText('People');
+		const description = getByText('sync-people-description');
+
+		expect(title).toBeInTheDocument();
+		expect(description).toBeInTheDocument();
+	});
+});
