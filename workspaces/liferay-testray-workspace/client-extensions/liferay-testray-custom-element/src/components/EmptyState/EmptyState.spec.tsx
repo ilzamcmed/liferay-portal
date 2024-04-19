f2666ac04af87453bb3fdc2fa1f9e946cb2ae7de
Ilza Medeiros <ilza.medeiros@liferay.com>
2024-04-19 15:49:53 -0300

    LPD-21742 - Replace empty state GIf for SVG

--- workspaces/liferay-testray-workspace/client-extensions/liferay-testray-custom-element/src/components/EmptyState/EmptyState.spec.tsx
+++ workspaces/liferay-testray-workspace/client-extensions/liferay-testray-custom-element/src/components/EmptyState/EmptyState.spec.tsx
@@ -14,7 +14,7 @@ describe('EmptyState', () => {
 		expect(queryByText('No results found')).toBeTruthy();
 		expect(queryByText('Sorry, there are no results found')).toBeTruthy();
 		expect(container.querySelector('img')?.src).toBe(
-			'http://localhost:3000/states/empty_state.gif'
+			'http://localhost:3000/states/empty_state.svg'
 		);
 		expect(asFragment()).toMatchSnapshot();
 	});
