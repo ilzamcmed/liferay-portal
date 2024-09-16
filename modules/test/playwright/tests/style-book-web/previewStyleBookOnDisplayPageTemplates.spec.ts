ae5510bec190eb2cf5ef7f3ea111f6ee4843a6eb
Ilza Medeiros <ilza.medeiros@liferay.com>
2024-09-16 19:56:35 -0300

    LPD-35557 - Second refactor to add other page templates and check selection on modal

--- modules/test/playwright/tests/style-book-web/previewStyleBookOnDisplayPageTemplates.spec.ts
+++ modules/test/playwright/tests/style-book-web/previewStyleBookOnDisplayPageTemplates.spec.ts
@@ -3,16 +3,14 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {Page, expect, mergeTests} from '@playwright/test';
+import {expect, mergeTests} from '@playwright/test';
 
 import {isolatedSiteTest} from '../../fixtures/isolatedSiteTest';
 import {loginTest} from '../../fixtures/loginTest';
 import {pageEditorPagesTest} from '../../fixtures/pageEditorPagesTest';
 import {styleBookPageTest} from '../../fixtures/styleBookPageTest';
 import {workflowPagesTest} from '../../fixtures/workflowPagesTest';
-import {PageEditorPage} from '../../pages/layout-content-page-editor-web/PageEditorPage';
-import {DisplayPageTemplatesPage} from '../../pages/layout-page-template-admin-web/DisplayPageTemplatesPage';
-import {StyleBooksPage} from '../../pages/style-book-web/StyleBooksPage';
+import {clickAndExpectToBeVisible} from '../../utils/clickAndExpectToBeVisible';
 import getRandomString from '../../utils/getRandomString';
 import {blogsPagesTest} from '../blogs-web/fixtures/blogsPagesTest';
 
@@ -25,46 +23,6 @@ const test = mergeTests(
 	workflowPagesTest
 );
 
-const changeStyleAndCheckPreview = async (
-	colorHEX: string,
-	colorRGB: string,
-	displayPageTemplatesPage: DisplayPageTemplatesPage,
-	page: Page,
-	pageEditorPage: PageEditorPage,
-	site: Site,
-	styleBooksPage: StyleBooksPage,
-	displayPageTemplateName: string
-) => {
-	await styleBooksPage.goto(site.friendlyUrlPath);
-	await styleBooksPage.editStyleBook('Test Style Book Name');
-
-	await page.getByRole('button', {name: 'Color System'}).click();
-	await page.getByRole('menuitem', {name: 'General'}).click();
-
-	await styleBooksPage.updateTokenInputColor('Body Color', colorHEX);
-
-	expect(page.getByText('Saved')).toBeVisible();
-
-	await styleBooksPage.publishStyleBook();
-
-	await displayPageTemplatesPage.goto(site.friendlyUrlPath);
-
-	await displayPageTemplatesPage.editTemplate(displayPageTemplateName);
-	await page.getByLabel('Page Design Options').click();
-	await page.getByRole('tab', {name: 'Style Book'}).click();
-	await page.getByLabel('Test Style Book Name').click();
-
-	const headingId = await pageEditorPage.getFragmentId('Heading');
-
-	const fragmentColor = await pageEditorPage.getFragmentStyle({
-		fragmentId: headingId,
-		isTopperStyle: true,
-		style: 'color',
-	});
-
-	expect(fragmentColor).toBe(colorRGB);
-};
-
 test('Add a heading fragment and view the display page templates is shown in preview type selector', async ({
 	displayPageTemplatesPage,
 	page,
@@ -72,81 +30,230 @@ test('Add a heading fragment and view the display page templates is shown in pre
 	site,
 	styleBooksPage,
 }) => {
-	const displayPageTemplateName = getRandomString();
+	const displayPageTemplateNameBlogsEntry = 'Blogs ' + getRandomString();
+	const displayPageTemplateNameCategory = 'Category ' + getRandomString();
+	const displayPageTemplateNameDocuments = 'Documents ' + getRandomString();
+	const displayPageTemplateNameWebContent1 = 'Web-1 ' + getRandomString();
+	const displayPageTemplateNameWebContent2 = 'Web-2 ' + getRandomString();
+	const styleBookName = getRandomString();
 
-	await test.step('Create style book and view the fragments is selected in the preview type selector', async () => {
-		await styleBooksPage.goto(site.friendlyUrlPath);
-		await styleBooksPage.createStyleBook('Test Style Book Name');
-
-		await styleBooksPage.publishStyleBook();
-
-		await styleBooksPage.editStyleBook('Test Style Book Name');
-		await expect(
-			page.getByRole('button', {name: 'Fragments'})
-		).toBeVisible();
-	});
-
-	await test.step('Create and publish a new page template for web content article and basic web content', async () => {
-		await displayPageTemplatesPage.goto(site.friendlyUrlPath);
-		await displayPageTemplatesPage.createTemplate({
-			contentSubtype: 'Basic Web Content',
-			contentType: 'Web Content Article',
-			name: displayPageTemplateName,
-		});
-
-		await displayPageTemplatesPage.editTemplate(displayPageTemplateName);
-
+	const placeHeadingFragmentAndSelectText = async () => {
 		await pageEditorPage.addFragment(
 			'Basic Components',
 			'Heading',
 			page.locator('#page-editor div').first()
 		);
 
-		const headingId = await pageEditorPage.getFragmentId('Heading');
+		await page.getByLabel('Browser').click();
+		await page.getByLabel('Select Heading').click();
+		await page.getByLabel('Select element-text').click();
+	};
 
-		expect(await pageEditorPage.isActive(headingId)).toBe(true);
+	await test.step('Create and publish a new page template for web content article and basic web content 1', async () => {
+		await displayPageTemplatesPage.goto(site.friendlyUrlPath);
 
-		await changeStyleAndCheckPreview(
-			'#666666',
-			'rgb(102, 102, 102)',
-			displayPageTemplatesPage,
-			page,
-			pageEditorPage,
-			site,
-			styleBooksPage,
-			displayPageTemplateName
+		await displayPageTemplatesPage.createTemplate({
+			contentSubtype: 'Basic Web Content',
+			contentType: 'Web Content Article',
+			name: displayPageTemplateNameWebContent1,
+		});
+
+		await displayPageTemplatesPage.editTemplate(
+			displayPageTemplateNameWebContent1
 		);
+
+		await placeHeadingFragmentAndSelectText();
+
+		await page.getByLabel('Field').selectOption('JournalArticle_title');
+
+		await displayPageTemplatesPage.publishTemplate();
 	});
 
-	await test.step('View the items shown in dropdown menu of preview item selector then change the preview item to the first display page template', async () => {
+	await test.step('Create and publish a new page template for web content article and basic web content 2', async () => {
+		await displayPageTemplatesPage.goto(site.friendlyUrlPath);
+
+		await displayPageTemplatesPage.createTemplate({
+			contentSubtype: 'Basic Web Content',
+			contentType: 'Web Content Article',
+			name: displayPageTemplateNameWebContent2,
+		});
+
+		await displayPageTemplatesPage.editTemplate(
+			displayPageTemplateNameWebContent2
+		);
+
+		await placeHeadingFragmentAndSelectText();
+
+		await page.getByLabel('Field').selectOption('JournalArticle_title');
+
+		await displayPageTemplatesPage.publishTemplate();
+	});
+
+	await test.step('Create and publish a new page template for blogs entry', async () => {
+		await displayPageTemplatesPage.goto(site.friendlyUrlPath);
+
+		await displayPageTemplatesPage.createTemplate({
+			contentType: 'Blogs Entry',
+			name: displayPageTemplateNameBlogsEntry,
+		});
+
+		await displayPageTemplatesPage.editTemplate(
+			displayPageTemplateNameBlogsEntry
+		);
+
+		await placeHeadingFragmentAndSelectText();
+
+		await page.getByLabel('Source').selectOption('structure');
+
+		await page.getByLabel('Field').selectOption('BlogsEntry_content');
+
+		await displayPageTemplatesPage.publishTemplate();
+	});
+
+	await test.step('Create and publish a new page template for document and basic document', async () => {
+		await displayPageTemplatesPage.goto(site.friendlyUrlPath);
+
+		await displayPageTemplatesPage.createTemplate({
+			contentSubtype: 'Basic Document',
+			contentType: 'Document',
+			name: displayPageTemplateNameDocuments,
+		});
+
+		await displayPageTemplatesPage.editTemplate(
+			displayPageTemplateNameDocuments
+		);
+
+		await placeHeadingFragmentAndSelectText();
+
+		await page.getByLabel('Source').selectOption('structure');
+
+		await page.getByLabel('Field').selectOption('FileEntry_authorName');
+
+		await displayPageTemplatesPage.publishTemplate();
+	});
+
+	await test.step('Create and publish a new page template for category', async () => {
+		await displayPageTemplatesPage.goto(site.friendlyUrlPath);
+
+		await displayPageTemplatesPage.createTemplate({
+			contentType: 'Category',
+			name: displayPageTemplateNameCategory,
+		});
+
+		await displayPageTemplatesPage.editTemplate(
+			displayPageTemplateNameCategory
+		);
+
+		await placeHeadingFragmentAndSelectText();
+
+		await page.getByLabel('Source').selectOption('structure');
+
+		await page.getByLabel('Field').selectOption('AssetCategory_name');
+
+		await displayPageTemplatesPage.publishTemplate();
+	});
+
+	await test.step('Create style book and view the display page template is selected in the preview type selector', async () => {
 		await styleBooksPage.goto(site.friendlyUrlPath);
 
-		await styleBooksPage.editStyleBook('Test Style Book Name');
+		await styleBooksPage.createStyleBook(styleBookName);
+		await styleBooksPage.publishStyleBook();
+
+		await styleBooksPage.editStyleBook(styleBookName);
+
+		await expect(
+			page.getByRole('button', {name: 'Display Page Templates'})
+		).toBeVisible();
+	});
+
+	await test.step('View the items shown in dropdown menu of preview item selector then change the preview item for each display page template', async () => {
+		const selectOptionAndUpdateColor = async (
+			selectButton: string,
+			selectMenuItem: string,
+			colorHEX: string,
+			colorRGB: string
+		) => {
+			await page.getByRole('button', {name: selectButton}).click();
+			await page.getByRole('menuitem', {name: selectMenuItem}).click();
+
+			await styleBooksPage.updateTokenInputColor('Body Color', colorHEX);
+
+			expect(page.getByText('Saved')).toBeVisible();
+
+			const heading = page
+				.frameLocator('iframe')
+				.getByRole('heading', {name: 'Heading Example'});
+
+			const fragmentColor = await heading.evaluate((element) => {
+				const computedStyle = window.getComputedStyle(element);
+
+				return computedStyle.color;
+			});
+
+			expect(fragmentColor).toBe(colorRGB);
+		};
 
 		await page.getByRole('button', {name: 'Color System'}).click();
 		await page.getByRole('menuitem', {name: 'General'}).click();
+
+		await selectOptionAndUpdateColor(
+			displayPageTemplateNameCategory,
+			displayPageTemplateNameCategory,
+			'#227777',
+			'rgb(34, 119, 119)'
+		);
+
+		await styleBooksPage.updateTokenInputColor('Body Color', '#227777');
+
+		expect(page.getByText('Saved')).toBeVisible();
+
+		await selectOptionAndUpdateColor(
+			displayPageTemplateNameCategory,
+			displayPageTemplateNameDocuments,
+			'#556622',
+			'rgb(85, 102, 34)'
+		);
+
+		await selectOptionAndUpdateColor(
+			displayPageTemplateNameDocuments,
+			displayPageTemplateNameBlogsEntry,
+			'#995511',
+			'rgb(153, 85, 17)'
+		);
+
+		await selectOptionAndUpdateColor(
+			displayPageTemplateNameBlogsEntry,
+			displayPageTemplateNameWebContent2,
+			'#666666',
+			'rgb(102, 102, 102)'
+		);
+	});
+
+	await test.step('View the more button to select a display page template from select modal and change preview', async () => {
+		await clickAndExpectToBeVisible({
+			autoClick: true,
+			target: page.getByRole('button', {name: 'More'}),
+			trigger: page.getByRole('button', {
+				name: displayPageTemplateNameWebContent2,
+			}),
+		});
+
+		await page
+			.locator('iframe[title="Select"]')
+			.contentFrame()
+			.getByRole('button', {
+				name: 'Select ' + displayPageTemplateNameWebContent1,
+			})
+			.click();
+
+		const loadingAnimation = page.locator('.loading-animation').first();
+		await loadingAnimation.waitFor();
+		await loadingAnimation.waitFor({state: 'hidden'});
 
 		await styleBooksPage.updateTokenInputColor('Body Color', '#005566');
 
 		expect(page.getByText('Saved')).toBeVisible();
 
 		await styleBooksPage.publishStyleBook();
-
-		await displayPageTemplatesPage.goto(site.friendlyUrlPath);
-
-		await page.getByRole('link', {name: displayPageTemplateName}).click();
-		await page.getByLabel('Page Design Options').click();
-		await page.getByRole('tab', {name: 'Style Book'}).click();
-		await page.getByLabel('Test Style Book Name').click();
-
-		const headingId = await pageEditorPage.getFragmentId('Heading');
-
-		const fragmentColor = await pageEditorPage.getFragmentStyle({
-			fragmentId: headingId,
-			isTopperStyle: true,
-			style: 'color',
-		});
-
-		expect(fragmentColor).toBe('rgb(0, 85, 102)');
 	});
 });
