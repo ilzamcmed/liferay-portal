3540456df1e0e8070f4cfe0a0a1d6e3c51763965
Ilza Medeiros <ilza.medeiros@liferay.com>
2024-09-25 13:53:46 -0300

    LPD-35557 - Rename search function for document library

--- modules/test/playwright/tests/document-library-web/fileEntry.spec.ts
+++ modules/test/playwright/tests/document-library-web/fileEntry.spec.ts
@@ -300,7 +300,7 @@ testSearchInDlPortlet(
 
 		await page.goto('/web' + site.friendlyUrlPath);
 
-		await documentLibraryPage.searchFor(title);
+		await documentLibraryPage.search(title);
 
 		await clickAndExpectToBeVisible({
 			autoClick: true,
